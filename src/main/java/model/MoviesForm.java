b014cd7d656e32e290fd27a7fa51ee655ac805dd
Marija Trajkova <tmarija2008@yahoo.com>
2019-07-31 10:43:33 +0200

    Add java class appropriate for the form

---
+++ src/main/java/model/MoviesForm.java
@@ -0,0 +1,40 @@
+package main.java.model;
+
+public class MoviesForm {
+    private String name;
+    private String surname;
+    private int age;
+    private String favoriteMovie;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFavoriteMovie() {
+        return favoriteMovie;
+    }
+
+    public void setFavoriteMovie(String favoriteMovie) {
+        this.favoriteMovie = favoriteMovie;
+    }
+}
